e893ab1a8994dcf56e084c2e3a9e14c54c641e47
Vish137 <83007697+Vish137@users.noreply.github.com>
2021-09-25 15:58:52 +1000

    Update CES.py

--- Portfolio Theory/CES.py
+++ Portfolio Theory/CES.py
@@ -3,15 +3,15 @@ def CES(L, K, gamma,*args):
     #CES parameterisation
     if gamma==0:
         raise ZeroDivisionError('gamma must be non-zero')
-    #TFP
-    A=10
     #Output elasticities
     if args:
-        if len(args)!=1:
-            raise ValueError('You need to indicate the value of output elasticity, a, only')
-        a=args[0]
+        if len(args)!=2:
+            raise ValueError('You need to indicate the value of total factor production and output elasticity, A and a, only')
+        A=args[0]
+        a=args[1]
         b=1-a
     else:
+        A=10
         a=0.5
         b=1-a
     return A * (a*L**(gamma) + b*K**(gamma))**(1/gamma)

6858aeebf294ee0d5f8f2170ecd40f8411f6afd8
Vish137 <83007697+Vish137@users.noreply.github.com>
2021-09-25 15:54:26 +1000

    Update and rename Production Function.ipynb to CES.py

---
+++ Portfolio Theory/CES.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+from mpl_toolkits.mplot3d import Axes3D
+def CES(L, K, gamma,*args):
+    #CES parameterisation
+    if gamma==0:
+        raise ZeroDivisionError('gamma must be non-zero')
+    #TFP
+    A=10
+    #Output elasticities
+    if args:
+        if len(args)!=1:
+            raise ValueError('You need to indicate the value of output elasticity, a, only')
+        a=args[0]
+        b=1-a
+    else:
+        a=0.5
+        b=1-a
+    return A * (a*L**(gamma) + b*K**(gamma))**(1/gamma)
+
